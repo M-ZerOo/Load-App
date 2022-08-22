6ea1bce295476f6cea4b80e87112a05da99b1872
Mahmoud <mahmoudelfoulyyy@gmail.com>
2022-08-22 18:54:46 +0200

    Initial commit

---
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -0,0 +1,176 @@
+package com.udacity
+
+import android.animation.ValueAnimator
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.LinearInterpolator
+import kotlin.properties.Delegates
+
+class LoadingButton @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    private var widthSize = 0
+    private var heightSize = 0
+
+    // Setting custom attributes and values for the button
+    private val attributes = context.obtainStyledAttributes(attrs, R.styleable.LoadingButton)
+
+    private val backgroundColorOnStart =
+        attributes.getColor(
+            R.styleable.LoadingButton_backgroundColor,
+            context.getColor(R.color.colorPrimary)
+        )
+    private val backgroundColorOnLoading = attributes.getColor(
+        R.styleable.LoadingButton_backgroundColor,
+        context.getColor(R.color.colorPrimaryDark)
+    )
+
+    private val circleColor = attributes.getColor(
+        R.styleable.LoadingButton_circleColor,
+        context.getColor(R.color.colorAccent)
+    )
+
+    private val textColor =
+        attributes.getColor(R.styleable.LoadingButton_textColor, context.getColor(R.color.white))
+
+    // Setting up text
+    private var text = context.getString(R.string.button_name)
+
+    // Storing progress of animation
+    private var completed = 0.0f
+    private var sweepAngle = 0.0f
+
+    private var circleRect = RectF()
+    private var progressRect = RectF()
+
+
+    private val valueAnimator = ValueAnimator.ofFloat(0f, 100f).apply {
+        duration = 1500
+        repeatCount = ValueAnimator.INFINITE
+        interpolator = AccelerateInterpolator()
+        addUpdateListener {
+            completed = animatedFraction
+            sweepAngle = 360f * completed
+            progressRect.right = widthSize * completed
+            if (buttonState == ButtonState.Completed) {
+                cancel()
+            }
+            super.invalidate()
+        }
+    }
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        isAntiAlias = true
+        color = backgroundColorOnStart
+    }
+
+    private val circlePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        isAntiAlias = true
+        strokeCap = Paint.Cap.ROUND
+        strokeJoin = Paint.Join.ROUND
+        color = circleColor
+    }
+
+    private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        isAntiAlias = true
+        textAlign = Paint.Align.CENTER
+        textSize = resources.getDimension(R.dimen.default_text_size)
+        color = textColor
+    }
+
+    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+        when (new) {
+            ButtonState.Clicked -> {
+                text = context.getString(R.string.button_name)
+            }
+
+            ButtonState.Loading -> {
+                text = context.getString(R.string.button_loading)
+                val textRect = Rect()
+                textPaint.getTextBounds(text, 0, text.length, textRect)
+                circleRect.set(
+                    widthSize / 2f + textRect.width() / 2f + textRect.height() / 2f,
+                    heightSize / 2f - textRect.height() / 2f,
+                    widthSize / 2f + textRect.width() / 2f + textRect.height() * 1.5f,
+                    heightSize / 2f + textRect.height() / 2f
+                )
+                valueAnimator.start()
+            }
+
+            ButtonState.Completed -> {
+                text = context.getString(R.string.button_name)
+                valueAnimator.end()
+                valueAnimator.setCurrentFraction(0f)
+            }
+        }
+    }
+
+
+    init {
+        isClickable = true
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        widthSize = w
+        heightSize = h
+
+        progressRect.bottom = heightSize.toFloat()
+    }
+
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawColor(backgroundColorOnStart)
+
+        canvas.drawText(
+            context.getString(R.string.button_name),
+            (widthSize / 2).toFloat(),
+            (heightSize / 2).toFloat(),
+            textPaint
+        )
+
+        if (buttonState == ButtonState.Loading) {
+            canvas.drawColor(backgroundColorOnLoading)
+            canvas.drawRect(progressRect, paint)
+            canvas.drawArc(circleRect, 270f, sweepAngle, true, circlePaint)
+            canvas.drawText(
+                context.getString(R.string.button_loading),
+                (widthSize / 2).toFloat(), (heightSize / 2).toFloat(), textPaint
+            )
+        }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val minW: Int = paddingLeft + paddingRight + suggestedMinimumWidth
+        val w: Int = resolveSizeAndState(minW, widthMeasureSpec, 1)
+        val h: Int = resolveSizeAndState(
+            View.MeasureSpec.getSize(w),
+            heightMeasureSpec,
+            0
+        )
+        widthSize = w
+        heightSize = h
+        setMeasuredDimension(w, h)
+    }
+
+    override fun performClick(): Boolean {
+        super.performClick()
+        if (buttonState == ButtonState.Loading) {
+            return false
+        }
+        return true
+    }
+
+    // To change the state of the button from the MainActivity
+    fun changeState(state: ButtonState) {
+        buttonState = state
+    }
+}
---
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -0,0 +1,156 @@
+package com.udacity
+
+import android.app.DownloadManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.widget.RadioGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var downloadID: Long = 0
+    private var usedUrl: String? = null
+
+    private lateinit var notificationManager: NotificationManager
+    //private lateinit var pendingIntent: PendingIntent
+    private lateinit var action: NotificationCompat.Action
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val radioGroup: RadioGroup = findViewById(R.id.radio_group)
+        val loadingButton: LoadingButton = findViewById(R.id.custom_button)
+
+        registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+
+        notificationManager =
+            getSystemService(NotificationManager::class.java) as NotificationManager
+        // create a channel for your notification
+        createChannel()
+
+        // Check which radio button is clicked and save the url based on it
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.glide_radio -> usedUrl = GLIDE_URL
+                R.id.load_app_radio -> usedUrl = UDACITY_URL
+                R.id.retrofit_radio -> usedUrl = RETROFIT_URL
+            }
+        }
+
+        loadingButton.setOnClickListener {
+            download()
+        }
+    }
+
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            // Return the button state to Completed once it's done
+            custom_button.changeState(ButtonState.Completed)
+            val id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+            Log.d("Download Status", "id: $id")
+
+            notificationManager =
+                getSystemService(NotificationManager::class.java) as NotificationManager
+
+            notificationManager.sendNotification(
+                context.getString(R.string.notification_description),
+                context
+            )
+        }
+    }
+
+    private fun download() {
+        // Check if no radio button clicked make a toast to select one then return from download function
+        if (usedUrl == null) {
+            Toast.makeText(this, "Please select the file to download.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // Call changeState method so the loading animation starts
+        custom_button.changeState(ButtonState.Loading)
+
+        val request =
+            DownloadManager.Request(Uri.parse(usedUrl))
+                .setTitle(getString(R.string.app_name))
+                .setDescription(getString(R.string.app_description))
+                .setRequiresCharging(false)
+                .setAllowedOverMetered(true)
+                .setAllowedOverRoaming(true)
+
+        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+        downloadID =
+            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+        Log.d("Download Status", "downloadID: $downloadID")
+    }
+
+    companion object {
+        private const val GLIDE_URL = "https://github.com/bumptech/glide"
+        private const val UDACITY_URL =
+            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter"
+        private const val RETROFIT_URL = "https://github.com/square/retrofit"
+        private const val CHANNEL_ID = "channelId"
+    }
+
+    private fun NotificationManager.sendNotification(
+        messageBody: String,
+        applicationContext: Context
+    ) {
+        val NOTIFICATION_ID = 0
+
+        val contentIntent = Intent(applicationContext, DetailActivity::class.java)
+        val contentPendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        action = NotificationCompat.Action.Builder(
+            R.drawable.ic_assistant_black_24dp,
+            applicationContext.getString(R.string.notification_button),
+            contentPendingIntent
+        ).build()
+
+        val builder = NotificationCompat.Builder(
+            applicationContext,
+            applicationContext.getString(R.string.load_notification_channel_id)
+        )
+            .setSmallIcon(R.drawable.ic_assistant_black_24dp)
+            .setContentTitle(applicationContext.getString(R.string.notification_title))
+            .setContentText(messageBody)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(contentPendingIntent)
+            .setAutoCancel(true)
+            .addAction(action)
+
+        notify(NOTIFICATION_ID, builder.build())
+    }
+
+    private fun createChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelName = getString(R.string.load_notification_channel_name)
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationChannel.enableVibration(true)
+
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+
+}
